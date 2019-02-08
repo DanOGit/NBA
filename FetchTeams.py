d24f4c2bb7a975134a3f96d4b3322d021e6b799b
DanOGit <doconnor62@comcast.net>
2019-02-08 11:55:01 -0500

    Add files via upload

---
+++ FetchTeams.py
@@ -0,0 +1,17 @@
+import requests
+
+NBA_TEAMS_URL = 'http://data.nba.net/10s//prod/v2/2018/teams.json'
+
+r = requests.get(NBA_TEAMS_URL)
+
+# print(r.content)
+
+fullData = r.json()
+teams = fullData['league']['standard']
+
+
+for item in teams:
+    if item['isNBAFranchise']:
+        print (item['fullName'])
+
+print(teams)
